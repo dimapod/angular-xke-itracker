80441acf1209d47baeb4cbb88d5e68a72ff34951
dimapod <dimapod@gmail.com>
2013-04-01 14:45:11 +0200

    EXO-3.2.x done

--- app/js/controllers/addController.js
+++ app/js/controllers/addController.js
@@ -2,12 +2,17 @@
 
 bugTrackerApp.controller('addCtrl', function ($scope, user, $location, issueResource) {
     // EXO-3.2.1 ($scope init)
+    $scope.readonly = false;
+    $scope.submitting = false;
+    $scope.issue = { date: new Date(), reporter: user.login, status: "new" }
 
     $scope.add = function () {
         if ($scope.submitting) return;
         $scope.submitting = true;
 
         // EXO-3.2.2 (save new issue & redirect to /issue)
+        issueResource.save($scope.issue);
+        $location.url('/issue');
     }
 
 });
