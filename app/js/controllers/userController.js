866f00a6e34aff0d13f297744abaf12bb1433d2a
dimapod <dimapod@gmail.com>
2013-04-01 14:37:33 +0200

    EXO-2.2.x done

--- app/js/controllers/userController.js
+++ app/js/controllers/userController.js
@@ -4,10 +4,15 @@
 bugTrackerApp.controller('userCtrl', function CustomerController($scope, $location, user) {
 
     // EXO-2.2.1 ($scope init)
+    $scope.login = user.login;
+    $scope.name = user.name;
 
     $scope.doLogin = function (login, name) {
         // EXO-2.2.2
+        user.login = login;
+        user.name = name;
 
         // EXO-2.2.3 ($location)
+        $location.url('/');
     };
 });
