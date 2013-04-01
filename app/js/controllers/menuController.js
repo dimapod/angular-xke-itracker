bb14399b5f04aa4d392b700cf534fdfa9cb5346f
dimapod <dimapod@gmail.com>
2013-04-01 14:28:55 +0200

    EXO-1.2.1 done

--- app/js/controllers/menuController.js
+++ app/js/controllers/menuController.js
@@ -5,6 +5,7 @@ bugTrackerApp.controller('menuCtrl', function ($scope, $location) {
 
     $scope.routeIs = function (routeName) {
         // EXO-1.2.1 (compare actual route (see $location) with routeName)
+        return $location.path() === routeName;
     };
 
 });
