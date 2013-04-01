1154d6b70eff7e577bab4cfcd319add23841b68b
dimapod <dimapod@gmail.com>
2013-04-01 14:53:23 +0200

    EXO-4.2 done

--- app/js/controllers/issueController.js
+++ app/js/controllers/issueController.js
@@ -2,6 +2,9 @@
 
 bugTrackerApp.controller('issueCtrl', function ($scope, user, $location, issueResource) {
     // EXO-4.3 (redirect to /user if not logged in)
+    if (!user.login) {
+        $location.url('/user');
+    }
 
     $scope.issues = issueResource.query();
 });
